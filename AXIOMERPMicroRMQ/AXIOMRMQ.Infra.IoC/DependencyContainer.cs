661b33a9a16d4b22683b1a5cfadef430172ed1fa
Suzauddaula Suza <suzauddaula103@gmail.com>
2021-07-30 22:07:24 +0600

    initial commit of infrastructure dependency project

---
+++ AXIOMERPMicroRMQ/AXIOMRMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using AXIOMMicroRMQ.Domain.Core.Bus;
+using AXIOMMicroRMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AXIOMRMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //Domain Bus
+            service.AddTransient<IEventBus, AXIOMRMQBus>();
+        }
+    }
+}
