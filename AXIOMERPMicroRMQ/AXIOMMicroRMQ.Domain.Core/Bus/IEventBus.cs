a3182f639f47579afb9319be4a5749096a642516
Suzauddaula Suza <suzauddaula103@gmail.com>
2021-07-30 20:34:37 +0600

    implementation of rabbitmq bus completed

--- AXIOMERPMicroRMQ/AXIOMMicroRMQ.Domain.Core/Bus/IEventBus.cs
+++ AXIOMERPMicroRMQ/AXIOMMicroRMQ.Domain.Core/Bus/IEventBus.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AXIOMMicroRMQ.Domain.Core.Commands;
+using AXIOMMicroRMQ.Domain.Core.Events;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
